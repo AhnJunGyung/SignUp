9b5a98b9ea531d0da2d327dc2504f972f9724435
AHNJUNGYUNG <ekdlrkzm@naver.com>
2025-04-18 16:58:20 +0900

    feat: 프로젝트 생성

---
+++ SignUp/Main/MainViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MainViewController.swift
+//  SignUp
+//
+//  Created by 안준경 on 4/18/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class MainViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    
+    //MARK: UI 컴포넌트
+    
+    private let startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("시작하기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    //MARK: UI 세팅
+
+    private func setUp() {
+        view.backgroundColor = .white
+        
+        view.addSubview(startButton)
+        
+        startButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.width.equalTo(150)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func tapButton() {
+        startButton.rx.tap
+            .subscribe(onNext: {
+                
+            }).disposed(by: disposeBag)
+    }
+    
+}
+
